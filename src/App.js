abb6dad5c2dfb561245679d4443a40b06ef2ca82
sinnedh <dennis.hellmich@me.com>
2018-02-07 12:48:10 +0100

    Split regular eand exceptional hours overview

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import gql from 'graphql-tag';
 import { Container, Menu } from 'semantic-ui-react'
 import { graphql } from 'react-apollo';
-import Overview from './components/Overview'
+import RegularHoursOverview from './components/RegularHoursOverview';
+import ExceptionalHoursOverview from './components/ExceptionalHoursOverview';
 import 'semantic-ui-css/semantic.min.css';
 
 const App = ({ data }) => (
-    <React.Fragment>
-        <Menu inverted fixed="top">
-            { !data.loading && data.Venue && <Menu.Item>{data.Venue.name}</Menu.Item> }
-            <Menu.Item>Regular hours</Menu.Item>
-            <Menu.Item>Exceptional hours</Menu.Item>
-        </Menu>
+    <Router>
+        <React.Fragment>
+            <Menu inverted fixed="top">
+                { !data.loading && data.Venue && <Menu.Item>{data.Venue.name}</Menu.Item> }
 
-        <Container style={{ marginTop: '5em' }} >
-            { !data.loading && data.Venue &&
-                <Overview
-                    regularHours={data.allRegularHourses}
-                    exceptionalHours={data.allExceptionalHourses}
-                />
-            }
-        </Container>
-    </React.Fragment>
+                <Menu.Item
+                    as={Link}
+                    to={'/'}
+                    name='regular_hours'>
+                    Regular hours
+                </Menu.Item>
+
+                <Menu.Item
+                    as={Link}
+                    to={'/exceptional_hours/'}
+                    name='exceptional_hours'>
+                    Exceptional hours
+                </Menu.Item>
+            </Menu>
+
+            <Container style={{ marginTop: '5em' }} >
+                { !data.loading && data.Venue &&
+                    <Switch>
+                        <Route exact path="/" render={() =>
+                            <RegularHoursOverview regularHours={data.allRegularHourses} />
+                        } />
+                        <Route path="/exceptional_hours" render={() =>
+                            <ExceptionalHoursOverview exceptionalHours={data.allExceptionalHourses} />
+                        } />
+                    </Switch>
+                }
+            </Container>
+        </React.Fragment>
+    </Router>
 )
 
 const query = gql`
--- src/components/ExceptionalHoursOverview.js
+++ src/components/ExceptionalHoursOverview.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {format} from 'date-fns'
 
 const ExceptionalHoursOverview = ({ exceptionalHours }) => (
-  <ul>
-    { exceptionalHours.map((day, key) =>
-      <li key={key}>
-        {format(day.open, 'YYYY-MM-DD: HH:mm')} - {format(day.close, 'HH:mm')}
-      </li>
-    )}
-  </ul>
+  <React.Fragment>
+    <h2>Exceptional hours</h2>
+    <ul>
+      { exceptionalHours.map((day, key) =>
+        <li key={key}>
+          {format(day.open, 'YYYY-MM-DD: HH:mm')} - {format(day.close, 'HH:mm')}
+        </li>
+      )}
+    </ul>
+  </React.Fragment>
 )
 
 export default ExceptionalHoursOverview;
