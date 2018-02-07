076088c84dc1465c4b13ab369b03a205cc2603eb
sinnedh <dennis.hellmich@me.com>
2018-02-07 18:31:06 +0100

    Use Grid for listing instead of li

--- src/components/ExceptionalHoursOverview.js
+++ src/components/ExceptionalHoursOverview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { format } from 'date-fns';
+import { Divider, Grid } from 'semantic-ui-react';
 import ExceptionalHoursCreate from './ExceptionalHoursCreate';
 import ExceptionalHoursDelete from './ExceptionalHoursDelete';
 
@@ -7,19 +8,30 @@ const ExceptionalHoursOverview = ({ exceptionalHours }) => (
   <React.Fragment>
     <h2>Exceptional hours</h2>
     <ExceptionalHoursCreate />
-    <ul>
-      { exceptionalHours.map((day, key) =>
-        <li key={key}>
-          <ExceptionalHoursDelete id={day.id} />
-          { format(day.open, 'YYYY-MM-DD') + ': ' }
-          { day.isClosed
-            ? 'Closed'
-            : <span>{format(day.open, 'HH:mm')} - {format(day.close, 'HH:mm')}</span>
-          }
-          { day.comment && ' ' + day.comment }
-        </li>
-      )}
-    </ul>
+
+    <Divider />
+    { exceptionalHours.map((day, key) =>
+      <React.Fragment>
+        <Grid>
+          <Grid.Column width={4}>
+            <strong>{ format(day.open, 'YYYY-MM-DD') }</strong>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            { day.isClosed
+              ? 'Closed'
+              : <span>{format(day.open, 'HH:mm')} - {format(day.close, 'HH:mm')}</span>
+            }
+          </Grid.Column>
+          <Grid.Column width={4}>
+            { day.comment && ' ' + day.comment }
+          </Grid.Column>
+          <Grid.Column width={2}>
+            <ExceptionalHoursDelete id={day.id} />
+          </Grid.Column>
+        </Grid>
+        <Divider />
+      </React.Fragment>
+    )}
   </React.Fragment>
 )
 
