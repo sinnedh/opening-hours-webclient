5a554395c6e458d1df6eee548c7ed4f5bc24ad2e
sinnedh <dennis.hellmich@me.com>
2018-02-13 22:07:39 +0100

    Stash 1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ import ExceptionalHoursOverview from './components/ExceptionalHoursOverview';
 import { fetchInitialDataQuery } from './queries';
 import 'semantic-ui-css/semantic.min.css';
 
+
+
+// TODO
+// + extract component
+// + implement isCloses handling
+// + isNowOpenQuery date is currently hardcoded
+import { isNowOpenQuery } from './queries';
+import { isNowOpen } from './datetimeUtils';
+
+const IsNowOpen = ({ data }) => {
+  if(data.allRegularHourses && data.allExceptionalHourses) {
+    const now = new Date()
+    return isNowOpen(now, data.allRegularHourses, data.allExceptionalHourses)
+      ? <Icon color="green" name="wait" size="large" />
+      : <Icon color="red" name="wait" size="large" />
+  }
+
+  return <span />
+}
+
+const IsNowOpenContainer = graphql(
+    isNowOpenQuery,
+    { options: { notifyOnNetworkStatusChange: true, } }
+)(IsNowOpen);
+
+
 const App = ({ data }) => (
     <Router>
         <React.Fragment>
